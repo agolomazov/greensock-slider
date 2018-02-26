5b7cb185a4575e2eaa3ef574f1448e4103ecf55f
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2018-02-26 08:56:17 +0300

    Change animation block

--- js/main.js
+++ js/main.js
@@ -18,10 +18,26 @@
       $projectImage = $project.find('.project-image');
 
   // Create a project timeline
-  tlProject.fromTo(
+  tlProject
+  .set([$projectTitle, $projectSubtitle, $pixel],
+    { autoAlpha: 0}
+  ).fromTo(
     $projectImage,
-    4,
+    0.4,
     { autoAlpha: 0, xPercent: '-200' },
-    { autoAlpha: 1, xPercent: '0', ease: Power4.easeInOut }
-  );
+    { autoAlpha: 1, xPercent: '-10', ease: Power4.easeInOut }
+  ).add('imageIn')
+  .staggerFromTo($pixel, 0.3, { autoAlpha: 0, x: '-=10' }, {autoAlpha: 1, x: '0', ease: Power4.easeInOut}, 0.02, '-=0.2')
+  .add('pixelsIn')
+  .fromTo($projectTitle, 0.7, {autoAlpha: 0, xPercent: '-50'}, { autoAlpha: 1, xPercent: '-5', ease: Power4.easeInOut}, '-=0.4')
+  .fromTo($projectSubtitle, 0.7, {autoAlpha: 0, xPercent: '-50'}, { autoAlpha: 1, xPercent: '-2', ease: Power4.easeInOut}, '-=0.5')
+  .add('titleIn')
+  .to($projectTitle, 4.3, { xPercent: '+=5', ease: Linear.easeNone }, 'titleIn-=0.1')
+  .to($projectSubtitle, 4.3, { xPercent: '+=2', ease: Linear.easeNone }, 'titleIn-=0.2')
+  .add('titleOut')
+  .to($projectImage, 5, { xPercent: '0', ease: Linear.easeNone }, 'imageIn')
+  .add('imageOut')
+  .to($pixels, 4.1, {x: '-5', ease: Linear.easeNone}, 'pixelsIn')
+  .to([$projectTitle, $projectSubtitle], 0.5, {autoAlpha: 0, xPercent: '+=10', ease: Power4.easeInOut}, 'titleOut')
+  .to($projectImage, 0.4, {autoAlpha: 0, xPercent: '+=80', ease: Power4.easeInOut}, 'imageOut');
 })(jQuery);
